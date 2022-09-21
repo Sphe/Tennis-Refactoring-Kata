b36b3bc27ab154756a86105bdde2424ca375e3b5
Alexandre Lazar <alexandre.lazar@gmail.com>
2022-09-21 20:10:23 +0200

    Creating Domain Object + Encapsulating Labeling Logic in ScoreLabelService Test passing 33 / 33

---
+++ csharp/Tennis/Domain/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis.Domain
+{
+    public class Player : IEquatable<Player>
+    {
+        
+        private string _name;
+        private int _score;
+
+        public Player(string name, int score = 0)
+        {
+            _name = name;
+            _score = score;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Score
+        {
+            get { return _score; }
+        }
+
+        public void Scoring()
+        {
+            _score++;
+        }
+
+        public bool Equals(Player other)
+        {
+            return string.Compare(_name, other.Name, true) == 0;
+        }
+
+    }
+}
---
+++ csharp/Tennis/Service/ScoreLabelService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tennis.Domain;
+
+namespace Tennis.Service
+{
+    public class ScoreLabelService
+    {
+
+        private string[] _labelEqualScore = new string[]
+        {
+            "Love-All",
+            "Fifteen-All",
+            "Thirty-All"
+        };
+
+        private string[] _labelScore = new string[]
+        {
+            "Love",
+            "Fifteen",
+            "Thirty",
+            "Forty"
+        };
+
+        public string Create(Player player1, Player player2)
+        {
+
+            var score1 = player1.Score;
+            var score2 = player2.Score;
+
+            var finalScore = string.Empty;
+
+            if (score1 == score2 && score1 >= 3 && score2 >= 3)
+            {
+                finalScore = "Deuce";
+            }
+            else if (score1 == score2)
+            {
+                finalScore = _labelEqualScore[score1];
+            }
+            else if (score1 >= 4 || score2 >= 4)
+            {
+                var minusResult = score1 - score2;
+
+                if (minusResult == 1)
+                {
+                    finalScore = "Advantage player1";
+                }
+                else if (minusResult == -1)
+                {
+                    finalScore = "Advantage player2";
+                }
+                else if (minusResult >= 2)
+                {
+                    finalScore = "Win for player1";
+                }
+                else
+                {
+                    finalScore = "Win for player2";
+                }
+
+            }
+            else
+            {
+                var i = 1;
+                while (i < 3)
+                {
+
+                    int tempScore;
+
+                    if (i == 1)
+                    {
+                        tempScore = score1;
+                    }
+                    else
+                    {
+                        tempScore = score2;
+                        finalScore = String.Concat(finalScore, "-");
+                    }
+
+                    finalScore = String.Concat(finalScore, _labelScore[tempScore]);
+                    i++;
+                }
+            }
+
+            return finalScore;
+
+        }
+
+    }
+}
